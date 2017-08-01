78f02d75eef0f9119e5f369f70c14524415921e7
andreasvija <andreasvija@hotmail.com>
2017-08-01 16:52:12 +0300

    Facebook login implemented

--- app/src/main/java/blendin/blendin/LoginActivity.java
+++ app/src/main/java/blendin/blendin/LoginActivity.java
@@ -1,13 +1,80 @@
 package blendin.blendin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends Activity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        AccessToken token = AccessToken.getCurrentAccessToken();
+
+        if (token != null) {
+            enterApp();
+        }
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        // If using in a fragment
+        //loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                AccessToken token = loginResult.getAccessToken();
+                Log.d("###debug", "facebook:onSuccess" + loginResult + token);
+
+                enterApp();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.d("###debug", "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.d("###debug", "facebook:onError", exception);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    void enterApp () {
+        Intent intent = new Intent(getApplicationContext(), CategoriesActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
