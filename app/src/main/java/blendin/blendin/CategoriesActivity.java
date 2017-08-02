3526338cf0e2996de1b4e875a22da8428b533e04
andreasvija <andreasvija@hotmail.com>
2017-08-02 23:48:52 +0300

    Categories layout and make categories responsive

--- app/src/main/java/blendin/blendin/CategoriesActivity.java
+++ app/src/main/java/blendin/blendin/CategoriesActivity.java
@@ -1,13 +1,46 @@
 package blendin.blendin;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class CategoriesActivity extends Activity {
+public class CategoriesActivity extends Activity implements View.OnClickListener{
+
+    int activeCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        activeCategory = R.id.category_all;
+        switchActiveCategory(activeCategory);
+
+        findViewById(R.id.category_all).setOnClickListener(this);
+        findViewById(R.id.category_finance).setOnClickListener(this);
+        findViewById(R.id.category_food).setOnClickListener(this);
+        findViewById(R.id.category_shopping).setOnClickListener(this);
+        findViewById(R.id.category_transport).setOnClickListener(this);
+        findViewById(R.id.category_travel).setOnClickListener(this);
+        findViewById(R.id.category_other).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        String s = getResources().getResourceEntryName(id);
+        Log.d("###", "onClick: " + s);
+        switchActiveCategory(id);
+    }
+
+    void switchActiveCategory(int newActive) {
+        LinearLayout ll = (LinearLayout) findViewById(activeCategory);
+        ll.setBackground(getResources().getDrawable(R.drawable.back_inactive));
+        activeCategory = newActive;
+        ll = (LinearLayout) findViewById(activeCategory);
+        ll.setBackground(getResources().getDrawable(R.drawable.back_active));
     }
 }
