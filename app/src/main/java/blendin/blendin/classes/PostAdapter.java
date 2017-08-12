c2ffaa1c8ec27b5dee699efec35434fae22ffeac
andreasvija <andreasvija@hotmail.com>
2017-08-12 18:13:34 +0300

    CategoriesView now dynamically displays posts

--- app/src/main/java/blendin/blendin/classes/PostAdapter.java
+++ app/src/main/java/blendin/blendin/classes/PostAdapter.java
@@ -4,15 +4,22 @@
 
 package blendin.blendin.classes;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import blendin.blendin.R;
+import blendin.blendin.activities.CategoriesActivity;
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
 
@@ -25,8 +32,27 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
     // Holds all necessary views
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
+        public View view;
+        public ImageView photoView;
+        public TextView nameView;
+        public TextView titleView;
+        //public TextView detailsView;
+        public TextView answersView;
+        public TextView timeAgoView;
+
+        public final Context context;
+
         public ViewHolder(View view) {
             super(view);
+
+            this.view = view;
+            photoView = (ImageView) view.findViewById(R.id.author_photo);
+            nameView = (TextView) view.findViewById(R.id.author_name);
+            titleView = (TextView) view.findViewById(R.id.title);
+            //detailsView = (TextView) view.findViewById(R.id.details);
+            answersView = (TextView) view.findViewById(R.id.comment_count);
+            timeAgoView = (TextView) view.findViewById(R.id.timestamp);
+            context = view.getContext();
         }
     }
 
@@ -38,17 +64,27 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_post, parent, false);
 
-        updateViewContent();
-
         return new PostAdapter.ViewHolder(view);
     }
 
-    // Fill existing view with new data - to save resources,
-    // RecyclerView only keeps a few fragment views
-    // and when the view is scrolled, changes the content of existing views
+    // Fill newly initialized or already existing view with new data
+    // (to save resources, RecyclerView only keeps a few fragment views
+    // and when the view is scrolled, changes the content of existing views)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        updateViewContent();
+
+        Post post = posts.get(position);
+
+        Picasso.with(holder.context)
+                .load(post.author.photoURL)
+                //.resize(width,height).noFade()
+                .into(holder.photoView);
+        holder.nameView.setText(post.author.name);
+        holder.titleView.setText(post.title);
+        holder.answersView.setText(String.valueOf(post.getCommentCount()) + " " + "answers");
+        CharSequence ago = DateUtils.getRelativeTimeSpanString(post.timestamp,
+                System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
+        holder.timeAgoView.setText(ago);
     }
 
     @Override
@@ -56,8 +92,4 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         return posts.size();
     }
 
-    // Updates the data in template or recycled view
-    private void updateViewContent() {
-        //TODO: modify existing view here
-    }
 }
--- app/src/main/java/blendin/blendin/classes/User.java
+++ app/src/main/java/blendin/blendin/classes/User.java
@@ -4,12 +4,15 @@
 
 package blendin.blendin.classes;
 
+import java.net.URL;
+
 public class User {
     public String id;
     public String name;
     public String photoURL;
 
-    public User(String id, String name, String photoURL) { //TODO: download name and photoURL from fb
+    public User(String id, String name, String photoURL) {
+        //TODO: download id, name and photoURL from fb
         this.id = id;
         this.name = name;
         this.photoURL = photoURL;
